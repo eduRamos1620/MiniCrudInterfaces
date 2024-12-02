69a641fc83f4e028f795e06253acf0754b75f66e
eduRamos1620 <eduardoramosrosales16@gmail.com>
2024-12-01 18:11:17 -0600

    Comodines en metodos genericos

--- src/com/eduardo/generics/EjemploGenericos.java
+++ src/com/eduardo/generics/EjemploGenericos.java
@@ -33,6 +33,14 @@ public class EjemploGenericos {
         List<ClientePremiun> clientesPremiun = fromArrayToList(new ClientePremiun[]{
                 new ClientePremiun("Indra", "Ramos")
         });
+
+        imprimirClientes(clientes);
+        imprimirClientes(clientesLista);
+        imprimirClientes(clientesPremiun);
+
+        System.out.println("Maximo entre 1, 9 y 4 es: " + maximo(1, 9, 4));
+        System.out.println("Maximo entre 5.5, 3.2, 7.8 es: " + maximo(5.5, 3.2, 7.8));
+        System.out.println("Maximo entre perro, gato, pez es: " + maximo("perro", "gato", "pez"));
     }
 
     //Metodo Generico que convierte Arrays a Listas
@@ -53,5 +61,20 @@ public class EjemploGenericos {
             System.out.println(elemento);
         }
         return Arrays.asList(c);
+    }
+
+    public static void imprimirClientes(List<? extends Cliente> clientes){
+        clientes.forEach(System.out::println);
+    }
+
+    public static <T extends Comparable<T>>  T maximo(T a, T b, T c){
+        T max = a;
+        if(b.compareTo(max) > 0){
+            max = b;
+        }
+        if(c.compareTo(max) > 0){
+            max = c;
+        }
+        return max;
     }
 }
